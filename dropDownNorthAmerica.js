36516816f7fce4aea2b40200ac3c8b31bf3d4bb7
julius-altmann-3333 <167089364+julius-altmann-3333@users.noreply.github.com>
2025-03-23 07:44:51 +0100

    Delete dropDownNorthAmerica.js

--- dropDownNorthAmerica.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("toggleDropdownNorthAmerica").addEventListener("click", function() {
-    var dropdown_north_america = document.getElementById("dropdownContentNorthAmerica");
-    dropdown_north_america.style.display = (dropdown_north_america.style.display === "none" || dropdown_north_america.style.display === "") ? "block" : "none";
-});
